8a3c3ca337effad0ac99907644e1dcf56c9253e7
Masum-Reza <careerhunter4280@gmail.com>
2023-09-14 20:53:48 -0700

    dynamically showed all cards and responsive ok

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,28 @@
+import { BsCurrencyDollar } from 'react-icons/bs'
+import { IoIosBookmarks } from 'react-icons/io'
+
 const Card = ({ course }) => {
-    console.log(course)
+    // console.log(course)
     let { course_name, credit, description, img, price } = course;
     return (
-        <div className="flex flex-col  justify-center p-5 border shadow-md shadow-gray-400 rounded-lg space-y-2">
-            <img src={img} alt="" />
-            <p className="font-semibold">{course_name}</p>
+        <div className="flex flex-col  justify-center p-4 border shadow-md shadow-gray-400 rounded-lg space-y-2">
+            <img className='h-full' src={img} alt="" />
+            <p className="font-semibold text-[#1C1B1B]">{course_name}</p>
+            <p className="text-xs text-start text-[#1C1B1B99]">{description}</p>
+
+            <div className="flex justify-between text-[#1C1B1B99] font-semibold">
+                <div className="flex justify-center items-center gap-1">
+                    <BsCurrencyDollar />
+                    <p>price : {price}</p>
+                </div>
+                <div className="flex justify-center items-center gap-2">
+                    <IoIosBookmarks />
+                    <p>credit : {credit}hr</p>
+                </div>
+            </div>
+
+            <button className='border bg-[#2F80ED] text-white py-1 rounded-md font-semibold'>Select</button>
+
         </div>
     )
 }
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
+
 const Courses = () => {
 
     const [courses, setCourses] = useState([])
@@ -12,7 +13,7 @@ const Courses = () => {
 
 
     return (
-        <div className=' w-[70%] text-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        <div className=' lg:w-[70%] text-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
             {
                 courses.map((course, index) => <Card key={index} course={course}/>)
             }
